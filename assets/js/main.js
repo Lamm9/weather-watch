d6ae8fe4580db1c59d0ac53d0a6f61b37d8654e8
Harrison <harrylamm@icloud.com>
2023-02-06 10:27:02 -0800

    added some stuff

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,10 @@
-var location = $('#searchbar');
+$(function () {
+    var location = $('#searchbar');
+    var monday = $('#monday');
+    var tuesday = $('#tuesday');
+    var wednesday = $('#wednesday');
+    var thursdsay = $('#thursday');
+    var friday = $('#friday')
+    var saturday = $('#saturday');
+    var sunday = $('#sunday');
+})
