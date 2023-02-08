0ef21606b3bc578d83301c72980b95b9c77c5c62
Harrison <harrylamm@icloud.com>
2023-02-07 20:42:34 -0800

    i need help

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,8 +7,15 @@ $(function () {
     var friday = $('#friday')
     var saturday = $('#saturday');
     var sunday = $('#sunday');
+    var lastSearch = location.value;
 
     var weatherURL = 'https://api.openweathermap.org/data/2.5/forecast?lat={400}&lon={400}&appid={2d71f66b3295a7a9148c6748d8cd3964}';
 
     fetch(weatherURL);
+
+    location.on('submit', function() {
+        localStorage.setItem('Last Search', lastSearch);
+
+        monday.$('<p>'+lastSearch+'</p>')
+    })
 })
